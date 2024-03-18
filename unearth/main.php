70ece0387b645c1ff7eca6c4e1fa85a1f636adb3
KsushaRu <ksseniya.ross@mail.ru>
2024-03-18 16:39:03 +0300

    Modul_05

---
+++ unearth/main.php
@@ -0,0 +1,416 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Unearth</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Загрузка...</span>
+    </div>
+  </div>
+  
+  <div class="site-wrap"  id="home-section">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+
+      
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center position-relative">
+          
+            
+            <div class="site-logo">
+              <a href="#" class="text-black"><span class="text-primary">Unearth</a>
+            </div>
+            
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Главная</a></li>
+                <li><a href="#services-section" class="nav-link">Услуги</a></li>
+              
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">О нас</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Команда</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Цены</a></li>
+                    <li class="has-children">
+                    </li>
+                  </ul>
+                </li>
+                <li><a href="#contact-section" class="nav-link">Контакты</a></li>
+
+                <li><a href="index.php" class="nav-link">Выход</a></li>
+              </ul>
+              </nav>
+          
+            </div>
+
+          <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+    
+    <div class="owl-carousel slide-one-item">
+      
+
+      
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/hero_3.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-7">
+              <h1 data-aos="fade-up" data-aos-delay="">Добро пожаловать в UnEarth</h1>  
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100"> крупнейший провайдер технического обслуживания и ремонта воздушных судов на территории РФ</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Летай с нами</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/hero_2.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-8">
+              <h1 data-aos="fade-up" data-aos-delay="">UnEarth в России!</h1>      
+              <p class="mb-5" data-aos="fade-up" data-aos-delay="100">надежное качество, конкурентная стоимость работ и высокая производительность труда</p>
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Летай с нами</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+
+    </div>
+    
+    <div class="site-section">
+      <div class="block__73694 mb-2" id="services-section">
+        <div class="container">
+          <div class="row d-flex no-gutters align-items-stretch">
+
+            <div class="col-12 col-lg-6 block__73422" style="background-image: url('images/img_1.jpg');" data-aos="fade-right" data-aos-delay="">
+            </div>
+
+            
+
+            <div class="col-lg-5 ml-auto p-lg-5 mt-4 mt-lg-0" data-aos="fade-left" data-aos-delay="">
+              <h2 class="mb-3 text-black"> Техника авиакомпаний</h2>
+              <p>«UNEARTH» выполняет оперативное и периодическое техническое обслуживание самолётов Boeing 737, Boeing 747, Boeing 777, Airbus A350, Airbus A330, Airbus A320Ceo/Neo и Superjet 100. Является единственной в России организацией с уникальными компетенциями в части выполнения тяжелых форм обслуживания</p>
+
+              <ul class="ul-check primary list-unstyled mt-5">
+                <li>Двигатели, обеспечивающие плавную и бесперебойную работу техники</li>
+                <li>Авиационные герметики для герметизации фюзеляжа</li>
+                <li>Измерители воздушного давления</li>
+                <li>Датчики, втулки, керамические подножки и многое другое</li>
+              </ul>
+
+              
+            </div>
+
+          </div>
+        </div>      
+      </div>
+
+
+      <div class="block__73694">
+        <div class="container">
+          <div class="row d-flex no-gutters align-items-stretch">
+
+            <div class="col-12 col-lg-6 block__73422 order-lg-2" style="background-image: url('images/img_2.jpg');" data-aos="fade-left" data-aos-delay="">
+            </div>
+
+            
+
+            <div class="col-lg-5 mr-auto p-lg-5 mt-4 mt-lg-0 order-lg-1" data-aos="fade-right" data-aos-delay="">
+              <h2 class="mb-3 text-black">Преимущества сотрудничества</h2>
+              <p>Компания активно внедряет лучшие передовые технологии технического обслуживания и ремонта авиационной техники. Можно с уверенностью сказать, что сегодня «Аэрофлот Техникс» – это надежное качество, конкурентная стоимость работ и высокая производительность труда</p>
+
+              <ul class="ul-check primary list-unstyled mt-5">
+                <li>Высокое качество комплектующих подтверждено официальными сертификатами</li>
+                <li>Оперативный подбор необходимых запчастей и ремкомплектов</li>
+                <li>Индивидуальные поставки комплектующих под заказ.</li>
+                <li>Круглосуточная поддержка</li>
+              </ul>
+
+            </div>
+
+          </div>
+        </div>      
+      </div>
+    </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="site-section" id="team-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-md-7 text-center">
+            <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
+              <h2>Наша команда</h2>
+              <p>Наша команда – это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций. </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Александра Смирнова</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Директор</span>
+              <div class="block-social-1">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Алексей Кучумов</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Менеджер</span>
+              <div class="block-social-1">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Валентина Гончарова</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Инженер</span>
+              <div class="block-social-1">
+                
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+
+
+    <section class="site-section bg-light" id="pricing-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center text-center">
+          <div class="col-md-7">
+            <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
+              <h2>Цены на тарифы</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="pricing">
+              <h3 class="text-center text-black">Базовый</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>5 000 ₽</span>в год</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Доступ к базовому каталогу авиатехники</li>
+                <li>Ограниченное количество заказов в месяц</li>
+                <li class="remove">Оплата за каждый заказ</li>
+                <li class="remove">Техническая поддержка по электронной почте</li>
+                <li class="remove">Возможность оплаты с помощью банковской карты</li>
+                
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-secondary btn-md">Оформить</a>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="pricing">
+              <h3 class="text-center text-black">Люкс</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>20 000 ₽</span> в год</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Доступ ко всему каталогу авиатехники</li>
+                <li>Неограниченное количество заказов</li>
+                <li>Возможность получения скидок от поставщиков</li>
+                <li>Техническая поддержка по телефону</li>
+                <li class="remove">Возможность оформления отсроченной оплаты</li>
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-primary btn-md text-white">Оформить</a>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="pricing">
+              <h3 class="text-center text-black">Премиум</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>75 000 ₽</span>в год</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Доступ ко всему каталогу авиатехники</li>
+                <li>Персональный менеджер</li>
+                <li>Бесплатная доставка заказов</li>
+                <li>Возможность получения эксклюзивных запчастей</li>
+                <li>Еженедельные обновления каталога </li>
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-secondary btn-md">Оформить</a>
+              </p>
+            </div>
+          </div>
+        </div>
+    
+    </section>
+
+
+  
+
+
+    <div class="site-section bg-light" id="contact-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center mb-5" data-aos="fade-up" data-aos-delay="">
+            <div class="block-heading-1">
+              <h2>Контакты</h2>
+              <div class="wrapper">
+            <br>
+                <div class="ico-wrap"> <i class="fa fa-map-marker ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Наш фактический адрес:</h4>
+                    <div class="aio-icon-description">
+                       г. Ярославль, ул. Свободы, д. 3, офис 228</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap"> <i class="fa fa-clock-o ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Время работы офиса:</h4>
+                    <div class="aio-icon-description">
+                        пн-пт с 9:00 до 18:00, без перерывов.</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap">
+                    <i class="fa fa-envelope ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Электронная почта:</h4>
+                    <div class="aio-icon-description">
+                        unearth@mail.ru</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap">
+                    <i class="fa fa-phone ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Наш Телефон:</h4>
+                    <div class="aio-icon-description">
+                        8 (812) 703-70-46</div>
+                </div>
+           
+                <span class="clearfix"></span>
+             <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-8">
+                <h2 class="footer-heading mb-4">UnEarth</h2>
+                <p>«UNEARTH» – это надежное качество, конкурентная стоимость работ и высокая производительность труда за счет оптимизации и внедрения инновационных решений в производственный процесс.</p>
+              </div>
+              <div class="col-md-4 ml-auto">
+                <h2 class="footer-heading mb-4">Компания</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#home-section" class="nav-link">Главная</a></li>
+                  <li><a href="#services-section" class="nav-link">Услуги</a></li>
+                  <li><a href="#team-section" class="nav-link">Команда</a></li>
+                  <li><a href="#pricing-section" class="nav-link">Цены</a></li>
+                  <li><a href="#contact-section" class="nav-link">Контакты</a></li>
+                </ul>
+              </div>
+
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p class="copyright"><small>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> by Rossiiskaya Kseniya </a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </small></p>
+            </div>
+
+    </footer>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
